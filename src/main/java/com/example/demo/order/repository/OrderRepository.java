6e3e9de9c6987ab86c3f8c181606c03c9e958de4
tjddhdl <tjddhdl@gmail.com>
2025-05-28 10:59:38 +0900

    !!!

--- src/main/java/com/example/demo/order/repository/OrderRepository.java
+++ src/main/java/com/example/demo/order/repository/OrderRepository.java
@@ -3,18 +3,13 @@ package com.example.demo.order.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import com.example.demo.order.entity.Order;
 
-import jakarta.transaction.Transactional;
-
 public interface OrderRepository extends JpaRepository<Order, Integer> {
 
-	@Transactional
-	@Modifying
 	@Query(value = "select * from tbl_order where member_no = :memberNo", nativeQuery = true)
 	List<Order> findAllByMemberNo(@Param("memberNo") int memberNo);
 }
